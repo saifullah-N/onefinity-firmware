778ba045b00028c02fc3dfc0c0f0e927781f9397
saifullah-N <n.mmsaifi16331@gmail.com>
2023-02-16 23:38:58 +0530

    fixed bug in reading json

--- src/py/bbctrl/Web.py
+++ src/py/bbctrl/Web.py
@@ -89,7 +89,7 @@ class CheckConfigurationHandler(bbctrl.APIHandler):
         Config = False
         try:
             output = subprocess.check_output(
-                '''cat config.json | grep -o '"initalConfig":.*,' | cut -d: -f2 | sed 's/[" ,]//g'|grep -o 'false\|true' ''', shell=True).decode()[0:-1]
+                '''cat /var/lib/bbctrl/config.json | grep -o '"initalConfig":.*,' | cut -d: -f2 | sed 's/[" ,]//g'|grep -o 'false\|true' ''', shell=True).decode()[0:-1]
             if(output == " true"):
                 Config = True
             elif (output == " false"):
