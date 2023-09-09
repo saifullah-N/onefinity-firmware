d90ff818c6f9e31384ac01f96beab39786e374e6
saifullah-N <n.mmsaifi16331@gmail.com>
2023-09-09 19:39:18 +0530

    Added admin general reset to reset machine defaults.

--- src/js/admin-general-view.js
+++ src/js/admin-general-view.js
@@ -6,26 +6,6 @@ const merge = require("lodash.merge");
 
 const config_defaults = require("../resources/onefinity_defaults.json");
 
-const variant_defaults = {
-    machinist_x35: require("../resources/onefinity_machinist_x35_defaults.json"),
-    woodworker_x35: require("../resources/onefinity_woodworker_x35_defaults.json"),
-    woodworker_x50: require("../resources/onefinity_woodworker_x50_defaults.json"),
-    journeyman_x50: require("../resources/onefinity_journeyman_x50_defaults.json")
-};
-
-const z_slider_defaults = {
-  "Z-16 Original": {
-    "travel-per-rev": 4,
-    "min-soft-limit": -133,
-    "max-velocity": 3,
-  },
-  "Z-20 Heavy Duty": {
-    "travel-per-rev": 10,
-    "min-soft-limit": -160,
-    "max-velocity": 7,
-  },
-}; 
-
 module.exports = {
     template: "#admin-general-view-template",
     props: [ "config", "state" ],
@@ -35,9 +15,6 @@ module.exports = {
           confirmReset: false,
           autoCheckUpgrade: true,
           reset_variant: "",
-          z_slider: "",
-          z_slider_variant:" ",
-          config:""
         };
     },
 
@@ -73,6 +50,7 @@ module.exports = {
 
                 try {
                     await api.put("config/save", config);
+                    await api.put("set-initial-config", { setup: true });
                     this.$dispatch("update");
                     SvelteComponents.showDialog("Message", {
                         title: "Success",
@@ -88,46 +66,24 @@ module.exports = {
             fileReader.readAsText(files[0]);
         },
         
-        next: async function() {
+        reset: async function () {
             const config = merge(
                 {},
                 config_defaults,
-                variant_defaults[this.reset_variant]
-                );
-                
-                try {
+                );    
+            config.initalConfig = false;
+            try {
                     await api.put("config/save", config);
                     this.confirmReset = false;
                     this.$dispatch("update");
-                    this.config= config
-                    this.z_slider = true;
-                } catch (error) {
-                    console.error("Restore failed:", error);
-                    alert("Restore failed");
+                    location.hash = "get-started";
+
+            } catch (error) {
+                    console.error("Reset failed:", error);
+                    alert("Reset failed");
                 }
             },
-            
-            set_z_slider: async function(){
-                 const z_variant = merge(
-                   {},
-                   this.config.motors[3],
-                   z_slider_defaults[this.z_slider_variant],
-                 );
-                 
-                this.config.motors[3] = z_variant;
-                 try {
-                   await api.put("config/save", this.config);
-                   this.$dispatch("update");
-                   SvelteComponents.showDialog("Message", {
-                       title: "Success",
-                       message: "Configuration restored",
-                    });
-                    this.z_slider = false;
-                 } catch (error) {
-                   console.error("Z slider failed:", error);
-                   alert("failed to set Z slider  ");
-                 }    
-            },
+
         check: function() {
             this.$dispatch("check");
         },
