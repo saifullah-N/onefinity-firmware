69f02db5bfee79bb0cc01c1ec3da632906684825
saifullah-N <n.mmsaifi16331@gmail.com>
2023-09-14 19:50:51 +0530

    Updating ?? operator for old Chromium support.

--- src/js/app.js
+++ src/js/app.js
@@ -324,7 +324,7 @@ module.exports = new Vue({
             const config = await api.get("config/load");
             const wifi = await api.get("wifi");
             const initalConfig = await api.get("check-initial-config");
-            this.initalConfig = initalConfig.initalConfig ?? false;
+            this.initalConfig = initalConfig.initalConfig ? initalConfig.initalConfig : false;
             update_object(this.config, config, true);
             this.config.full_version = fixup_version_number(this.config.full_version);
             this.config.ip = wifi.ipAddresses;
